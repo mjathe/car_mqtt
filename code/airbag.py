06347c67529d5167486b898d0a759987540ffe66
mjathe <mjathe@users.noreply.github.com>
2020-07-09 02:29:07 +0200

    .

--- code/airbag.py
+++ code/airbag.py
@@ -4,7 +4,7 @@ import time
 import sys
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe(("/car/airbag", 2))
+    client.subscribe(("/car/reason", 2))
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
@@ -18,26 +18,24 @@ client.connect("localhost", 1882, 60)
 client.loop_start()
 while True:
     x = input("Something happend?\nha = heart attack\na = accident\naf = accident_fire\nao = accident_oil\nla = light_accident\nhard = hard_accident\np = police\nam = ambulance\nh = hospital\nn = None\n")
-    #time.sleep(3)
-    #if randint(0, 100) >50:
-    #    client.publish("/car/airbag", "accident")
+
     if x == "ha":
-        client.publish("/car/airbag", "heart_attack")
+        client.publish("/car/reason", "heart_attack")
     elif x == "a":
-        client.publish("/car/airbag", "accident")
+        client.publish("/car/reason", "accident")
     elif x == "af":
-        client.publish("/car/airbag", "accident_fire")
+        client.publish("/car/reason", "accident_fire")
     elif x == "ao":
-        client.publish("/car/airbag", "accident_oil")
+        client.publish("/car/reason", "accident_oil")
     elif x == "la":
-        client.publish("/car/airbag", "light_accident")
+        client.publish("/car/reason", "light_accident")
     elif x == "hard":
-        client.publish("/car/airbag", "hard_accident")
+        client.publish("/car/reason", "hard_accident")
     elif x == "p":
-        client.publish("/car/airbag", "police")
+        client.publish("/car/reason", "police")
     elif x == "am":
-        client.publish("/car/airbag", "ambulance")
+        client.publish("/car/reason", "ambulance")
     elif x == "h":
-        client.publish("/car/airbag", "hospital")
+        client.publish("/car/reason", "hospital")
     elif x == "n":
-        client.publish("/car/airbag", "None")
+        client.publish("/car/reason", "None")
--- code/local_broker.py
+++ code/local_broker.py
@@ -7,49 +7,56 @@ import time
 #starts local mqtt broker
 brokerport = str(1882)
 broker = subprocess.Popen("D:\Sciebo\STUDIUM\Bachelorarbeit\mosquitto\mosquitto -p "+brokerport, shell=False)
-print("Broker auf port "+brokerport+" gestartet.")
+print("Broker on port "+brokerport+" started.")
 
 data =[]
 connected = 1
+
 id ="1564654"
 name="Michael"
-location=[22.0,28.0]
+location=[0,0]
 
-def request(reasons):
-    call = {
-        "driver_name": name,
-        "location": location, #Array mit Koordinaten als floats
-        "reasons": reasons, #Array mit reasons als Strings
-        "id": id
-        }
-    client_o.publish("/hshl/users/"+id, json.dumps(call))
 def register():
     reg = {
         "driver_name": name,
-        "location": location, #Array mit Koordinaten als floats
+        "location": location,
         "reasons": "None",
         "id": id
         }
     client_o.publish("/hshl/users/", json.dumps(reg))
     client_o.subscribe("/hshl/users/"+id+"/data",2)
+
+def request(reasons):
+    call = {
+        "driver_name": name,
+        "location": location,
+        "reasons": reasons,
+        "id": id
+        }
+    client_o.publish("/hshl/users/"+id, json.dumps(call))
+
+
+
 #local client methods
 def on_connect_local(client, userdata, flags, rc):
     print("Connected to localbroker with rc:"+str(rc))
     client_l.subscribe(("/car/temp", 2))
-    client_l.subscribe(("/car/airbag", 2))
+    client_l.subscribe(("/car/reason", 2))
     client_l.subscribe(("/car/gps", 2))
+
 def on_message_local(client, userdata, msg):
     global location
-    if str(msg.topic) == "/car/airbag":
-        #client_o.publish("/hshl/users/"+id, name+" 22.2296756,28.0122287 accident "+id)
+    if str(msg.topic) == "/car/reason":
+
         request(msg.payload.decode())
-        #print("Accident!"+msg.topic+" "+str(msg.payload))
-        #store all incoming data FR A.1
+
+
     elif str(msg.topic) == "/car/gps":
         location = json.loads(msg.payload)
-        #print(location)
+
     else:
         print("new temperature"+msg.topic+" "+str(msg.payload))
+        #store all incoming data FR A.1
         data.append(str(msg.payload))
 
 #online client methods
@@ -78,9 +85,18 @@ def on_message_online(client, userdata, msg):
         js = json.loads(msg.payload)
         print("cardata from onlinebroker:")
         print(js)
+        if js==data:
+            data = []
     else:
         js = json.loads(msg.payload)
         print(js)
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print("Unexpected disconnection.")
+        client.reconnect()
+
+
 
 #initialise client for local broker
 client_l = mqtt.Client()
@@ -91,6 +107,7 @@ client_l.on_message = on_message_local
 client_o = mqtt.Client()
 client_o.on_connect = on_connect_online
 client_o.on_message = on_message_online
+client_o.on_disconnect = on_disconnect
 client_o.username_pw_set("solace-cloud-client", "nbsse0pkvpkvhpeh3ll5j7rpha")
 
 #connect to local brokers
@@ -100,11 +117,11 @@ client_o.connect("mr2mbqbl71a4vf.messaging.solace.cloud", 20614, 60)
 #start client loops
 client_l.loop_start()
 client_o.loop_start()
+
 while True:
     if connected:
         #send collected data FR D
         client_o.publish("/hshl/users/"+id+"/data", json.dumps(data))
-        data = []
         time.sleep(8)
     else:
         print("no connection to onlinebroker")
